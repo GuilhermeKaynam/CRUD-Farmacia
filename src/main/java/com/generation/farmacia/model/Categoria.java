93cbc65a5d6e6b6da0fe081c6a38cbc85b0f4607
Guilherme-Kaynam <guilhermekaynam2006@gmail.com>
2024-12-16 10:31:55 -0300

    Adicionando Categorias

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,10 +1,12 @@
 package com.generation.farmacia.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
